83dd6fe4481a99ca6634be10d3ba709026abb9cd
leoly521 <leoly521@163.com>
2015-03-18 12:06:04 +0800

    a

--- uros-parent/mvn-build.cmd
+++ uros-parent/mvn-build.cmd
@@ -5,6 +5,6 @@ cd %~dp0
 rem call mvn clean
 ping -n 2 127.0.0.1 > nul
 @echo on
-call mvn install -Dmaven.test.skip
+call mvn install deploy -Dmaven.test.skip
 
 if '%1' equ '' pause
--- uros/src/main/java/com/bamboocloud/uros/Constants.java
+++ uros/src/main/java/com/bamboocloud/uros/Constants.java
@@ -2,7 +2,7 @@ package com.bamboocloud.uros;
 
 public final class Constants {
 	
-	public static final String name = "Uros";
+	public static final String name = "UROS";
 	public static final String version = "1.0";
 	
 }
